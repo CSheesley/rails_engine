ab3c2718cf2ea5547a380dc8cc1adf8cfce91546
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-30 07:10:06 -0600

    Adds passing tests for api/v1/merchants index and show - along with route and controller actions

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,7 +4,26 @@ describe 'Merchant API' do
 
   it 'sends a list of merchants' do
     create_list(:merchant, 3)
-    binding.pry
+
+    get "/api/v1/merchants"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(3)
+  end
+
+  it 'can get a single merchant by id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(merchant["id"]).to eq(id)
   end
 
 end
