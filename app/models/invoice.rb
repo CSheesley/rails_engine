dafffe49a67001eefce2cdf641a92585e7942baf
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-01 17:51:49 -0600

    Merchant most items and revenue methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,9 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
-  has_one :invoice_item
   has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 
   enum status: [:shipped, :not_shipped]
 end
