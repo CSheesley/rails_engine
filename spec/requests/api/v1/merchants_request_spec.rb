f1ecddec00ebeced8c3c8092cfd4d4a07f66d284
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-30 20:39:29 -0600

    Merchants find all requests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,9 +9,9 @@ describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body)["data"]
 
-    expect(merchants["data"].count).to eq(3)
+    expect(merchants.count).to eq(3)
   end
 
   it 'can get a single merchant by id' do
@@ -19,11 +19,11 @@ describe 'Merchant API' do
 
     get "/api/v1/merchants/#{id}"
 
-    merchant = JSON.parse(response.body)
+    merchant = JSON.parse(response.body)["data"]
 
     expect(response).to be_successful
 
-    expect(merchant["data"]["attributes"]["id"]).to eq(id)
+    expect(merchant["attributes"]["id"]).to eq(id)
   end
 
   it 'can find a single merchant by name param' do
@@ -33,8 +33,8 @@ describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body)
-    expect(merchant["data"]["attributes"]["name"]).to eq(amazon.name)
+    merchant = JSON.parse(response.body)["data"]
+    expect(merchant["attributes"]["name"]).to eq(amazon.name)
   end
 
   it 'can find a single merchant by id param' do
@@ -44,8 +44,8 @@ describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body)
-    expect(merchant["data"]["attributes"]["id"]).to eq(amazon.id)
+    merchant = JSON.parse(response.body)["data"]
+    expect(merchant["attributes"]["id"]).to eq(amazon.id)
   end
 
   it 'can find a single merchant by date created at' do
@@ -55,9 +55,9 @@ describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body)
+    merchant = JSON.parse(response.body)["data"]
 
-    expect(merchant["data"]["attributes"]["name"]).to eq(amazon.name)
+    expect(merchant["attributes"]["name"]).to eq(amazon.name)
   end
 
   it 'can find a single merchant by date updated at' do
@@ -67,12 +67,11 @@ describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body)
+    merchant = JSON.parse(response.body)["data"]
 
-    expect(merchant["data"]["attributes"]["name"]).to eq(amazon.name)
+    expect(merchant["attributes"]["name"]).to eq(amazon.name)
   end
 ##########
-
   it 'can find all merchants by id param' do
     amazon = create(:merchant, name:"Amazon")
     ebay = create(:merchant, name:"eBay")
@@ -81,13 +80,59 @@ describe 'Merchant API' do
     get "/api/v1/merchants/find_all?id=#{ebay.id}"
 
     expect(response).to be_successful
-    
-    merchant = JSON.parse(response.body)
-    expected = merchant["data"].first["attributes"]["name"]
+
+    merchant = JSON.parse(response.body)["data"]
+    expected = merchant.first["attributes"]["name"]
 
     expect(expected).to eq(ebay.name)
   end
 
-end
+  it 'can find all merchants by name param' do
+    amazon = create(:merchant, name:"Amazon")
+    ebay = create(:merchant, name:"eBay")
+    etsy = create(:merchant, name:"Etsy")
 
-# mom_pop = create(:merchant, name:"Mom & Pop", created_at: 8.days.ago, updated_at: 2.days.ago)
+    get "/api/v1/merchants/find_all?name=#{etsy.name}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)["data"]
+    expected = merchant.first["attributes"]["name"]
+
+    expect(expected).to eq(etsy.name)
+  end
+
+  it 'can find all merchants by a created_at param' do
+    created_at_param = "2012-03-27T14:54:05.000Z"
+    amazon = create(:merchant, name:"Amazon", created_at: created_at_param )
+    ebay = create(:merchant, name:"eBay", created_at: created_at_param )
+    etsy = create(:merchant, name:"Etsy")
+
+    get "/api/v1/merchants/find_all?created_at=#{created_at_param}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)["data"]
+
+    expect(merchants.class).to eq(Array)
+    expect(merchants.count).to eq(2)
+  end
+
+  it 'can find all merchants by a updated_at param' do
+    updated_at_param = "2013-03-27T14:54:05.000Z"
+    amazon = create(:merchant, name:"Amazon", updated_at: updated_at_param )
+    ebay = create(:merchant, name:"eBay")
+    etsy = create(:merchant, name:"Etsy", updated_at: updated_at_param)
+    craigslist = create(:merchant, name:"Craigslist", updated_at: updated_at_param)
+
+    get "/api/v1/merchants/find_all?updated_at=#{updated_at_param}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)["data"]
+
+    expect(merchants.class).to eq(Array)
+    expect(merchants.count).to eq(3)
+  end
+
+end
