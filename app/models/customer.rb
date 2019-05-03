7a20e27c556a30db3fdbab812c8aec43ccc7cf3c
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-03 15:18:23 -0600

    Adds favorite cusotmer method by merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,15 @@
 class Customer < ApplicationRecord
   has_many :invoices
 
+
+  def self.favorite_customer(merchant)
+
+    Customer.select("customers.*, COUNT(transactions.id) as total")
+      .joins(invoices: :transactions)
+      .where("invoices.merchant_id = ?", merchant)
+      .merge(Transaction.successful)
+      .group(:id)
+      .order("total DESC")
+      .take
+  end
 end
