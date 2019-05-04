d668685a358ebd3002dbb32344f32cb6b949b38d
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-04 13:01:22 -0600

    Adds model relationship validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'Relationships' do
     it { should have_many :invoices}
+    it { should have_many(:transactions).through :invoices}
   end
 end
